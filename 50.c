6700cca03e5895902b551b8b5ea21b0535148609
Thanmaya Garimella <112673461+thanmaya1@users.noreply.github.com>
2023-01-30 07:37:39 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Swap two values
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+// Get a random pivot index
+int getPivot(int low, int high) {
+  srand(time(NULL));
+  return low + rand() % (high - low + 1);
+}
+
+// Partition array around pivot
+int partition(int arr[], int low, int high) {
+  int pivotIndex = getPivot(low, high);
+  int pivotValue = arr[pivotIndex];
+  swap(&arr[pivotIndex], &arr[high]);
+  int i = low - 1;
+  for (int j = low; j <= high - 1; j++) {
+    if (arr[j] < pivotValue) {
+      i++;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+  swap(&arr[i + 1], &arr[high]);
+  return i + 1;
+}
+
+// Quicksort function
+void quicksort(int arr[], int low, int high) {
+  if (low < high) {
+    int pivot = partition(arr, low, high);
+    quicksort(arr, low, pivot - 1);
+    quicksort(arr, pivot + 1, high);
+  }
+}
+
+// Main function
+int main() {
+  int arr[] = {10, 7, 8, 9, 1, 5};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  quicksort(arr, 0, n - 1);
+  for (int i = 0; i < n; i++)
+    printf("%d ", arr[i]);
+  return 0;
+}
